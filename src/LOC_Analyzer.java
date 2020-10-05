94f6c40c62632a001939fd7807d3d569a6ba2a1c
Pticrix <xargath@hotmail.com>
2020-10-05 01:40:55 -0400

    Quelques modifications, enlevé les regex inutiles, ajouté un todo, c'est le temps du dodoz.

--- src/LOC_Analyzer.java
+++ src/LOC_Analyzer.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 //    Plus besoin d'anticiper le whitespace.
 
 public class LOC_Analyzer {
-    private ArrayList<Classe> listClasses = new ArrayList<>();
+    private final ArrayList<Classe> listClasses = new ArrayList<>();
 
     // Métriques
     private int classe_LOC = 0;
@@ -21,42 +21,13 @@ public class LOC_Analyzer {
     // Misc
     private int import_lines_in_file = 0;
 
-    // Liste de patterns regex
-
-    // juste des String.contains pourrait peut-être être suffisant ici?
-    private final Pattern patternMultiLineCommentonOneLine = Pattern.compile("/\\*.*\\*/");
-    private final Pattern patternMultiLineCommentStart = Pattern.compile("/\\*");
-    private final Pattern patternMultiLineEnd = Pattern.compile("\\*/");
-    private final Pattern patternJavaDocStart = Pattern.compile("/\\*\\*"); // même "End" que MultiLine
-    private final Pattern patternSingleLineComment = Pattern.compile("//");
-
-    // Patrons pour classes et méthodes
-    private final Pattern classPattern = Pattern.compile(
-            "((public|protected|default|private)?\\s+)?" +
-            "class\\s+[a-zA-Z_$][a-zA-Z0-9_$]*\\s+((extends|implements)\\s+[a-zA-Z_$][a-zA-Z0-9_$]*\\s+)?\\{", Pattern.CASE_INSENSITIVE);
-            // à partir d'ici il faut compter les {} pour arriver à quelque chose d'équilibré.
-
-    private final Pattern methodPattern = Pattern.compile(
-            "((public|protected|default|private)\\s+)?" +
-            "(static\\s+)?" +
-            "[a-zA-Z_$][a-zA-Z0-9_$]*\\s+" +
-            "[a-zA-Z_$][a-zA-Z0-9_$]*\\s*" +
-            "\\((([a-zA-Z_$][a-zA-Z0-9_$]*(\\[])*\\s+[a-zA-Z_$][a-zA-Z0-9_$]*,)*\\s*" +
-            "([a-zA-Z_$][a-zA-Z0-9_$]*(\\[])*\\s+[a-zA-Z_$][a-zA-Z0-9_$]*))?\\)\\{", Pattern.CASE_INSENSITIVE);
-            // à partir d'ici, il faut compter les {} pour arriver à quelque chose d'équilibré.
-
     // Patrons misc
     private final Pattern javaNamingConvention = Pattern.compile("[a-zA-Z_$][a-zA-Z0-9_$]*");
 
-    private final Pattern importPattern = Pattern.compile("" +
-            "\\s.*import\\s[a-zA-Z_$][a-zA-Z0-9_$]*\\.[a-zA-Z_$][a-zA-Z0-9_$]*" +
-            "|^import\\s[a-zA-Z_$][a-zA-Z0-9_$]*\\.[a-zA-Z_$][a-zA-Z0-9_$]*");
-
     public LOC_Analyzer(ArrayList<String> fileLines){
 
-        boolean lineIsInMultiLinecomment = false;
-
         int javadocLines = 0;
+        int importLines = 0;
 
         for(int i = 0; i<fileLines.size(); ++i){
 
@@ -67,6 +38,10 @@ public class LOC_Analyzer {
 
             // Détection des classes et des méthodes
 
+            Pattern classPattern = Pattern.compile(
+                    "((public|protected|default|private)?\\s+)?" + "class\\s+[a-zA-Z_$][a-zA-Z0-9_$]*\\s+" +
+                            "((extends|implements)\\s+[a-zA-Z_$][a-zA-Z0-9_$]*\\s+)?\\{", Pattern.CASE_INSENSITIVE);
+                            // à partir d'ici il faut compter les {} pour arriver à quelque chose d'équilibré.
 
             Matcher classMatcher = classPattern.matcher(line);
             if (classMatcher.find()) {
@@ -74,14 +49,21 @@ public class LOC_Analyzer {
                 int classEnd = findBalancedCurlyBracket(i, fileLines);
                 if(javadocLines > 0){
                     listClasses.add(new Classe(className, i, classEnd, javadocLines));
+                    javadocLines = 0; // on remet le compteur à 0 puisque javadocs assignés.
                 } else {
                     listClasses.add(new Classe(className, i, classEnd, 0));
                 }
             }
 
+            Pattern methodPattern = Pattern.compile(
+                    "((public|protected|default|private)\\s+)?" +
+                            "(static\\s+)?" + "[a-zA-Z_$][a-zA-Z0-9_$]*\\s+" +  "[a-zA-Z_$][a-zA-Z0-9_$]*\\s*" +
+                            "\\((([a-zA-Z_$][a-zA-Z0-9_$]*(\\[])*\\s+[a-zA-Z_$][a-zA-Z0-9_$]*,)*\\s*" +
+                            "([a-zA-Z_$][a-zA-Z0-9_$]*(\\[])*\\s+[a-zA-Z_$][a-zA-Z0-9_$]*))?\\)\\{", Pattern.CASE_INSENSITIVE);
+                            // à partir d'ici, il faut compter les {} pour arriver à quelque chose d'équilibré.
+
             Matcher methodMatcher = methodPattern.matcher(line);
             if (methodMatcher.find()) {
-
                 Pattern pat = Pattern.compile("[a-zA-Z_$][a-zA-Z0-9_$]*\\s*\\(([a-zA-Z_$][a-zA-Z0-9_$\\s,<>\\[\\]]*)?\\)");
                 Matcher match = pat.matcher(methodMatcher.group());
                 String methodNameAndSign = match.group();
@@ -89,30 +71,33 @@ public class LOC_Analyzer {
                 // on assume que la méthode trouvée se trouve dans la dernière classe trouvée.
                 if(javadocLines > 0){
                     listClasses.get(listClasses.size() - 1).addMethod(methodNameAndSign, new Methode(methodNameAndSign, i, methodEnd, javadocLines));
-                    javadocLines = 0;
+                    javadocLines = 0; // on remet le compteur à 0 puisque javadocs assignés.
                 } else {
                     listClasses.get(listClasses.size() - 1).addMethod(methodNameAndSign, new Methode(methodNameAndSign, i, methodEnd, 0));
                 }
             }
 
-            //
 
-            Matcher importMatch = importPattern.matcher(line);
-            Matcher mlCommentOneLine = patternMultiLineCommentonOneLine.matcher(line);
+            // Détection des commentaires
 
             boolean mlCommentFound = false;
             boolean javaDocfound = false;
             boolean singleCommentFound = false;
 
+            Pattern patternMultiLineCommentonOneLine = Pattern.compile("/\\*.*\\*/");
+
+            // on vérifie que "//" n'est pas imbriqué dans un commentaire /* ... */ ou /* ...
+            // juste des String.contains pourrait peut-être être suffisant ici?
             if(line.contains("//") &&
-                    // on vérifie que "//" n'est pas imbriqué dans un commentaire /* ... */ ou /* ...
                     line.replaceAll(patternMultiLineCommentonOneLine.pattern(), "").
                     replaceAll("/\\*", "").contains("//")) {
 
+                // on enlève la partie commentée et on continue l'analyse sur ce qu'il reste.
                 line = line.replaceAll("//.*", "");
                 singleCommentFound = true;
             }
 
+            Matcher mlCommentOneLine = patternMultiLineCommentonOneLine.matcher(line);
             if(line.contains("/*")){
                 if(mlCommentOneLine.find()){
                     singleCommentFound = true;
@@ -121,11 +106,22 @@ public class LOC_Analyzer {
                     if(line.contains("/**")){
                         javaDocfound = true;
                     } else {
-                        lineIsInMultiLinecomment = true;
+                        mlCommentFound = true;
                     }
                     line = line.replaceAll("/\\*.*", "");
                 }
             }
+
+            // Détection des importations. On va ajouter ces lignes à toutes les classes présentes dans le fichier.
+
+            // todo : on pourrait laisser faire et juste dire que tout code ou commentaire qui ne se trouve pas dans
+            //  une classe en particulier appartient à toutes les classes se trouvant dans le fichier (surtout que
+            //  généralement, on retrouve une seule classe par fichier, du haut de mon peu d'expérience). Donc, on doit
+            //  détecter si on est dans une classe, et si non, garder en mémoire le compte de LOC / CLOC, et l'ajouter
+            //  à toutes les classes du fichier à la fin.
+            Pattern importPattern = Pattern.compile("\\s.*import\\s[a-zA-Z_$][a-zA-Z0-9_$]*\\.[a-zA-Z_$][a-zA-Z0-9_$]*"
+                    + "|^import\\s[a-zA-Z_$][a-zA-Z0-9_$]*\\.[a-zA-Z_$][a-zA-Z0-9_$]*");
+            Matcher importMatch = importPattern.matcher(line);
 
         }
     }
