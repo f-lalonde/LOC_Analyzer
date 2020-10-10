3074a4787ccc10b423ccce3bc15c810218fce24e
Pticrix <xargath@hotmail.com>
2020-10-10 00:50:08 -0400

    moved stuff around

--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo getting to work...
-@echo this may take a few minutes if run from the root of a large project.
-@echo off
-java -jar .\Analyzer-1.0-SNAPSHOT.jar 
-@echo on
-@echo work DONE!
-@echo off
-cls
-pause
